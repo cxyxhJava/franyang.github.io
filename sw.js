9b4c94c4f99b09c509ce8802f8190f95d133ca2d
yangxiaobo <Xiaobo.Yang5@geely.com>
2019-02-18 17:12:09 +0800

    标签修改

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ const getFixedUrl = (req) => {
 
   // 1. fixed http URL
   // Just keep syncing with location.protocol 
-  // fetch(httpURL) belongs to active mixed content. 
+  // fetch(httpURL) belongs to active mixed content. ˙
   // And fetch(httpRequest) is not supported yet.
   url.protocol = self.location.protocol
 
